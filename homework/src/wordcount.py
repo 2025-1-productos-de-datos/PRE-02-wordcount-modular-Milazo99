e1607f4926a5ff9967b3d71f3df1e64c52db514e
Camilo Montoya <camilom@solenium.co>
2025-05-03 10:14:53 -0500

    Plus: modularization

---
+++ homework/src/wordcount.py
@@ -0,0 +1,31 @@
+import os
+from .write_count_words import write_count_words
+
+def read_all_lines():
+    
+    all_lines = []
+    input_file_list = os.listdir('data/input/')
+
+    for filename in input_file_list:
+        with open(filename, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+            all_lines.extend(lines)
+    
+    return all_lines
+
+def main():
+
+    input_file_list = os.listdir('data/input/')
+
+    counter = {}
+    for filename in input_file_list:
+        with open('data/input/'+filename) as f:
+            for l in f:
+                for w in l.split( ):
+                    w = w.lower().strip(",.!?")
+                    counter[w] = counter.get(w, 0) + 1
+    
+    write_count_words(counter)
+
+if __name__ == "__main__":
+    main()
